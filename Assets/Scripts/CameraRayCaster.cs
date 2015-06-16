e2c027efe2a642dfb276b32f3b023b69b86ba189
HenchmenResources <buy@vortexdimensions.com>
2015-06-16 13:33:09 -0600

    Map complete track script update

---
+++ Assets/Scripts/CameraRayCaster.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRayCaster : MonoBehaviour {
+	private Vector3 mouseLocation;
+	//LevelActiveCheck will need to be replaced with code that checks the master level unlocked list.
+	private bool LevelActiveCheck = true;
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WorldMapMaster levelActiveScript = GameObject.Find ("Main Camera").GetComponent<WorldMapMaster> ();
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				WorldMapLightup LightScript = GameObject.Find ("lightManager").GetComponent<WorldMapLightup> ();
+				if (hit.rigidbody != null) {
+					//Trigger Dollhouse Lights
+					if (hit.collider.gameObject.name.Contains ("Level")) {
+						LightScript.buttonName = hit.collider.gameObject.name;
+					} else {
+						LightScript.buttonName = "Bob";
+					}
+					
+				}
+			}
+			
+			
+			
+			//LOAD APPROPRIATE LEVEL IF IT IS AVAILABLE
+
+			if (Input.GetMouseButtonDown (0)) {
+			if (hit.collider.gameObject.name == "Level1" && levelActiveScript.bActiveLevel1) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level2" && levelActiveScript.bActiveLevel2) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level3" && levelActiveScript.bActiveLevel3) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level4" && levelActiveScript.bActiveLevel4) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level5" && levelActiveScript.bActiveLevel5) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level6" && levelActiveScript.bActiveLevel6) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level7" && levelActiveScript.bActiveLevel7) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level8" && levelActiveScript.bActiveLevel8) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level9" && levelActiveScript.bActiveLevel9) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level10" && levelActiveScript.bActiveLevel10) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level11" && levelActiveScript.bActiveLevel11) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			if (hit.collider.gameObject.name == "Level12" && levelActiveScript.bActiveLevel12) {
+					Application.LoadLevel (hit.collider.gameObject.name);
+				}
+			}
+		}
+
+}
--- Assets/Scripts/WorldMapMaster.cs
+++ Assets/Scripts/WorldMapMaster.cs
@@ -22,6 +22,23 @@ public class WorldMapMaster : MonoBehaviour {
 	public bool bActiveLevel11 = false;
 	public bool bActiveLevel12 = false;
 
+	//Setup Instancing
+	private static WorldMapMaster instance = null;
+	public static WorldMapMaster Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -31,61 +48,7 @@ public class WorldMapMaster : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		if (Physics.Raycast (ray, out hit)){
-			WorldMapLightup LightScript = lightManager.GetComponent<WorldMapLightup> ();
-			if (hit.rigidbody != null){
-				//Trigger Dollhouse Lights
-				if (hit.collider.gameObject.name.Contains("Level")){
-					LightScript.buttonName = hit.collider.gameObject.name;
-				}else{
-					LightScript.buttonName = "Bob";
-				}
 
-			}
-		}
-		//LOAD APPROPRIATE LEVEL IF IT IS AVAILABLE
-		nextLevel = "bActive"+hit.collider.gameObject.name;
-		Debug.Log (nextLevel);
-		if (Input.GetMouseButtonDown (0)) {
-			if (hit.collider.gameObject.name == "Level1" && bActiveLevel1){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level2" && bActiveLevel2){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level3" && bActiveLevel3){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level4" && bActiveLevel4){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level5" && bActiveLevel5){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level6" && bActiveLevel6){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level7" && bActiveLevel7){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level8" && bActiveLevel8){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level9" && bActiveLevel9){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level10" && bActiveLevel10){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level11" && bActiveLevel11 ){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-			if (hit.collider.gameObject.name == "Level12" && bActiveLevel12){
-				Application.LoadLevel(hit.collider.gameObject.name);
-			}
-		}
 	}
 
 
