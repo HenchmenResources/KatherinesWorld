da9d81a1529744f8782674532806ccef04912034
Zak Larsen <buy@vortexdimensions.com>
2015-06-15 23:32:32 -0600

    World Map Script updates and keyframe animation

---
+++ Assets/Scripts/WorldMapLightup.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldMapLightup : MonoBehaviour {
+
+	public Light[] ActiveLight;
+	//isLightActive is used for testing if thelevel has been unlocked
+	//This will need to be removed and replaced with code to check the
+	//master level unlock variables
+	public bool[] isLightActive;
+	public string buttonName;
+	private string lightName;
+	private int lightIndex;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lightName = "DH_Light_" + buttonName;
+		for (int i = 0; i < ActiveLight.Length; i++) {
+			if (ActiveLight[i].name == lightName && isLightActive[i]) {
+				ActiveLight[i].intensity = 1.0f;
+				ActiveLight[i].range = 5f;
+			}else if (ActiveLight[i].name != lightName && isLightActive[i]){
+				ActiveLight[i].intensity = 0.5f;
+				ActiveLight[i].range = 2f;
+			}else{
+				ActiveLight[i].intensity = 0.0f;
+				ActiveLight[i].range = 1f;
+			}
+		}
+	}
+
+	void OnMouseEnter() {
+		Debug.Log ("Enter");
+	}
+
+	void OnMouseOver() {
+		Debug.Log ("Over");
+	}
+	
+	void OnMouseExit() {
+		Debug.Log ("Exit");
+	}
+
+}
--- Assets/Scripts/WorldMapMaster.cs
+++ Assets/Scripts/WorldMapMaster.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class WorldMapMaster : MonoBehaviour {
 	private Vector3 mouseLocation;
+	//LevelActiveCheck will need to be replaced with code that checks the master level unlocked list.
+	private bool LevelActiveCheck = true;
+	public GameObject lightManager;
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,30 +15,29 @@ public class WorldMapMaster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//if (Input.GetMouseButtonDown(0)) {
-		//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		//Physics.Raycast(Camera.main.transform.position, Input.mousePosition, 100);
-		//GameObject hitObject = RaycastHit.transform.parent;
-		//Debug.Log (hitObject.name);
-		//}
-		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)){
-				if (hit.rigidbody != null){
-					Debug.Log (hit.collider.gameObject.name);
-					Application.LoadLevel(hit.collider.gameObject.name);
+
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)){
+			WorldMapLightup LightScript = lightManager.GetComponent<WorldMapLightup> ();
+			if (hit.rigidbody != null){
+				//Trigger Dollhouse Lights
+				if (hit.collider.gameObject.name.Contains("Level")){
+					LightScript.buttonName = hit.collider.gameObject.name;
+				}else{
+					LightScript.buttonName = "Bob";
 				}
+
 			}
-			
-			
+		}
+		//LOAD APPROPRIATE LEVEL IF IT IS AVAILABLE
+
+		if (Input.GetMouseButtonDown (0) && LevelActiveCheck) {
+			if (hit.collider.gameObject.name.Contains("Level")){
+				Application.LoadLevel(hit.collider.gameObject.name);
+			}
 		}
 	}
+
+
 }
-
-
-//Vector3 fwd = transform.TransformDirection (Vector3.forward);
-//if (Physics.Raycast (Transform.position, fwd, 10)) {
-//	GameObject hitObject = RaycastHit.transform.gameObject;
-//	Debug.Log (hitObject.name);
-//}
