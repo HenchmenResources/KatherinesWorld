713175eb6c710ab2e0f8db3efea2d0d43076e175
Andrew Roberts <sombreroman55@gmail.com>
2015-06-23 11:14:08 -0600

    Updated DraggableObject script
    Updated script allows boxes to move with moving platforms. Attach to
    pushable objects.

--- Assets/Scripts/DrewTests/DraggableObject.cs
+++ Assets/Scripts/DrewTests/DraggableObject.cs
@@ -3,35 +3,12 @@ using System.Collections;
 
 public class DraggableObject : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Awake () 
+    void OnCollisionStay(Collision hit)
     {
-
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-	
-	}
-
-    void OnTriggerEnter(Collider c)
-    {
-        if (c.gameObject.tag == "Player")
-            GetComponent<Collider>().attachedRigidbody.isKinematic = true;
-    }
-
-    void OnTriggerStay(Collider c)
-    {
-        if (c.gameObject.tag == "Player")
-        {
-            Debug.Log("I am being triggered!");
-        }
-    }
-
-    void OnTriggerExit()
-    {
-        GetComponent<Collider>().attachedRigidbody.isKinematic = true;
+        Collider c = hit.collider;
+        if (c.gameObject.tag == "FreezeEffectHorizontal" || c.gameObject.tag == "FreezeEffectVertical" || c.gameObject.tag == "FreezeEffectDiagonalPos" || c.gameObject.tag == "FreezeEffectDiagonalNeg")
+            transform.parent = c.transform;
+        else
+            transform.parent = null;
     }
 }
